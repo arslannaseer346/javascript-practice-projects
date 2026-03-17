0988479099228114742b638ccc1cc71e1aa22c01
arslannaseer346 <arslannaseer346@gmail.com>
2026-03-17 22:39:12 +0500

    elements ids gets

---
+++ To-Do-App/script.js
@@ -0,0 +1,10 @@
+const inputtext = document.getElementById('text')
+const button = document.getElementById('btnadd')
+const ul = document.getElementById('addlist')
+button.addEventListener('click', () => {
+
+    let value = inputtext.value;
+    const  = document.createElement('li');
+    const button = document.createElement('button');
+
+})
