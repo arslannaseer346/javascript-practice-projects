e16028c0a128ac47fdb927ab2f8dc36069651931
arslannaseer346 <arslannaseer346@gmail.com>
2026-03-17 23:58:59 +0500

    edit button added

--- To-Do-App/script.js
+++ To-Do-App/script.js
@@ -1,24 +1,36 @@
 const inputtext = document.getElementById('text')
 const button = document.getElementById('btnadd')
 const ul = document.getElementById('addlist')
+let editlirefrence = null;
 button.addEventListener('click', function () {
     let inputvalue = inputtext.value
-    if (!inputvalue == "") {
+    if (inputvalue == "") {
+        alert("please fill the input");
+        return;
+    }
+    if (editlirefrence !== null) {
+        editlirefrence.firstChild.textContent = inputvalue;
+        editlirefrence = null;
+    }
+    else {
 
         const createli = document.createElement('li')
         const delbtn = document.createElement('button')
+        const editbtn = document.createElement('button')
         createli.innerText = inputvalue;
-        delbtn.innerText = "X";
+        editbtn.innerText = "Edit";
+        delbtn.innerText = "Delete";
         ul.appendChild(createli);
+        createli.appendChild(editbtn);
         createli.appendChild(delbtn);
-        inputtext.value = ""
+
         delbtn.addEventListener('click', function () {
             createli.remove()
         })
+        editbtn.addEventListener('click', function () {
+            inputtext.value = createli.firstChild.textContent;
+            editlirefrence = createli;
+        })
     }
-    else {
-        alert("please fill the input");
-    }
-
-
+    inputtext.value = ""
 })
