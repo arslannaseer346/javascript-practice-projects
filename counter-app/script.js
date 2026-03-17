a55456c4db2a3cd86c7f28d129234fe341aa2e35
arslannaseer346 <arslannaseer346@gmail.com>
2026-03-17 17:06:59 +0500

    js alert added

--- counter-app/script.js
+++ counter-app/script.js
@@ -4,8 +4,13 @@ function updateUI() {
     label.innerText = count;
 }
 document.getElementById('btn1').addEventListener('click', () => {
-    count--;
-    updateUI();
+    if (count === 0) {
+        alert("The count cannot be negative")
+    } else {
+
+        count--;
+        updateUI();
+    }
 })
 document.getElementById('btn2').addEventListener('click', () => {
     count = 0;
