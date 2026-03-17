2497a2dc16e13a7c006f4c4853e0ddd3c7a1fe26
arslannaseer346 <arslannaseer346@gmail.com>
2026-03-17 16:32:32 +0500

    modified js code

--- counter-app/script.js
+++ counter-app/script.js
@@ -1,17 +1,17 @@
-let count = 0;
-const counter = document.getElementById("counter");
-
-function increment() {
-    count++;
-    counter.innerText = count;
+let count = 0
+const label = document.getElementById('count')
+function updateUI() {
+    label.innerText = count;
 }
-
-function decrement() {
+document.getElementById('btn1').addEventListener('click', () => {
     count--;
-    counter.innerText = count;
-}
-
-function reset() {
+    updateUI();
+})
+document.getElementById('btn2').addEventListener('click', () => {
     count = 0;
-    counter.innerText = count;
-}
+    updateUI();
+})
+document.getElementById('btn3').addEventListener('click', () => {
+    count++;
+    updateUI();
+})
