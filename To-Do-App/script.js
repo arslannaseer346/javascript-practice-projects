754ead499553c436e736b50940e734fd582fc4b5
arslannaseer346 <arslannaseer346@gmail.com>
2026-03-17 23:03:37 +0500

    add and delete functionality added

--- To-Do-App/script.js
+++ To-Do-App/script.js
@@ -1,10 +1,24 @@
 const inputtext = document.getElementById('text')
 const button = document.getElementById('btnadd')
 const ul = document.getElementById('addlist')
-button.addEventListener('click', () => {
+button.addEventListener('click', function () {
+    let inputvalue = inputtext.value
+    if (!inputvalue == "") {
 
-    let value = inputtext.value;
-    const  = document.createElement('li');
-    const button = document.createElement('button');
+        const createli = document.createElement('li')
+        const delbtn = document.createElement('button')
+        createli.innerText = inputvalue;
+        delbtn.innerText = "X";
+        ul.appendChild(createli);
+        createli.appendChild(delbtn);
+        inputtext.value = ""
+        delbtn.addEventListener('click', function () {
+            createli.remove()
+        })
+    }
+    else {
+        alert("please fill the input");
+    }
+
 
 })
